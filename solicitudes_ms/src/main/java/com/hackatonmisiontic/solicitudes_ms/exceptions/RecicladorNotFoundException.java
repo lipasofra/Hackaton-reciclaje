5aecbd873aab0f5d29bd172b52847f33322b2315
juancruedagonzalez@gmail.com <juancruedagonzalez@gmail.com>
2021-12-12 11:31:51 -0500

    código solicitudMS

---
+++ solicitudes_ms/src/main/java/com/hackatonmisiontic/solicitudes_ms/exceptions/RecicladorNotFoundException.java
@@ -0,0 +1,7 @@
+package com.hackatonmisiontic.solicitudes_ms.exceptions;
+
+public class RecicladorNotFoundException extends  RuntimeException{
+    public RecicladorNotFoundException(String message) {
+        super(message);
+    }
+}
